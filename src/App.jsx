c1211e71bac90acfba6b20cdc68ccc5bc309115a
rb58853 <rb58853@gmail.com>
2024-02-09 13:13:38 -0500

    First

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Card from "./components/Card.jsx";
+
+function App() {
+  return <div className="App">
+    <h1>Hello</h1>
+    <div className='container'>
+      <Card />
+    </div>
+  </div>;
+}
+
+export default App;
